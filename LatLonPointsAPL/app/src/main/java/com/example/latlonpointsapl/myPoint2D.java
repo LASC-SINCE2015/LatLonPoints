fa54f1da25974c868f35f62f885b11dcdf868403
Lasc.since2015 <lasc.k2.since2015@gmail.com>
2020-05-08 07:10:53 +0900

    一旦UP

---
+++ LatLonPointsAPL/app/src/main/java/com/example/latlonpointsapl/myPoint2D.java
@@ -0,0 +1,10 @@
+package com.example.latlonpointsapl;
+
+public class myPoint2D {
+    public double x,y;
+
+    public myPoint2D(double x,double y){
+            this.x = x;
+            this.y = y;
+    }
+}
